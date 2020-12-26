597f7a2d8be534ac00bd350415afc806d68dfac6
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-12-26 13:52:46 +0100

    Project created using spring initializr. It includes Eukera Server as dependency

---
+++ eureka-server/src/main/java/io/builders/poc/alvaromerinogarcia/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package io.builders.poc.alvaromerinogarcia.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
