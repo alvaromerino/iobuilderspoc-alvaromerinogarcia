fba6e2cc8b757f076b6cfaf21854e53f627c22bc
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-12-27 11:39:32 +0100

    Enable eureka client

--- users-app/users-app-starter/src/main/java/io/builders/poc/alvaromerinogarcia/usersapp/UsersAppLauncher.java
+++ users-app/users-app-starter/src/main/java/io/builders/poc/alvaromerinogarcia/usersapp/UsersAppLauncher.java
@@ -3,9 +3,11 @@ package io.builders.poc.alvaromerinogarcia.usersapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableEurekaClient
 public class UsersAppLauncher {
     public static void main(String[] args) {
         SpringApplication.run(UsersAppLauncher.class);
