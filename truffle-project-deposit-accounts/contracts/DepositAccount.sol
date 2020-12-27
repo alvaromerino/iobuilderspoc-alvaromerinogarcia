c7dd65cbce5f8e79791c1429f8df3c38f6c67e65
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-12-27 14:59:15 +0100

    DepositAccount contract

---
+++ truffle-project-deposit-accounts/contracts/DepositAccount.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DepositAccount {
+    address private owner;
+    
+    /// @dev Only the owner can withdraw from this contract
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    /// @dev The requested withdrawal amount must be available in the contract balance
+    modifier withMinBalance(uint256 _amount) {
+        require(address(this).balance >= _amount);
+        _;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    /// @dev Allow deposits from anyone
+    function() external payable {}
+    
+    /// @dev Full withdrawal
+    function withdraw() public payable onlyOwner {
+        msg.sender.transfer(address(this).balance);
+    }
+    
+    /// @dev Partial withdrawal
+    /// @param _amount Amount requested for withdrawal
+    function withdraw(uint256 _amount) public payable onlyOwner withMinBalance(_amount) {
+        msg.sender.transfer(_amount);
+    }
+
+}
