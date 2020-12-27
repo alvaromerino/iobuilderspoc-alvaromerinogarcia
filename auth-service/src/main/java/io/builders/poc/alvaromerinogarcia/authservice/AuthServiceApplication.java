4e91ee88ebceb9c331307089dab5873176e06724
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-12-27 13:57:47 +0100

    Enable hystrix (circuit breaker) and sleuth (distributed tracing/logging)

--- auth-service/src/main/java/io/builders/poc/alvaromerinogarcia/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/io/builders/poc/alvaromerinogarcia/authservice/AuthServiceApplication.java
@@ -6,8 +6,10 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
--- auth-service/src/main/java/io/builders/poc/alvaromerinogarcia/authservice/security/UserDetailsServiceImpl.java
+++ auth-service/src/main/java/io/builders/poc/alvaromerinogarcia/authservice/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package io.builders.poc.alvaromerinogarcia.authservice.security;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,9 +14,15 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService  {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
 	@Autowired
 	private BCryptPasswordEncoder encoder;
@@ -28,25 +33,43 @@ public class UserDetailsServiceImpl implements UserDetailsService  {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-    	String role = "ROLE_USER";
-    	
+		LOGGER.info("Loading by username...");
+
+		String role = "ROLE_USER";
+		Map userMap = null;
+
+		LOGGER.info("Getting user info from users microservice...");
+		userMap = getUserInfoFromUsersService(username);
+
+		if (userMap != null && userMap.values().size() > 0) {
+			List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                    .commaSeparatedStringToAuthorityList(role);
+
+            // The "User" class is provided by Spring and represents a model class for user to be returned by UserDetailsService
+			// And used by auth manager to verify and check user authentication.
+			LOGGER.info("Returning user with username " + username);
+            return new User((String) userMap.get("username"), encoder.encode((String) userMap.get("password")), grantedAuthorities);
+		}
+
+		LOGGER.info("User with user " + username + " not found");
+        // If user not found. Throw this exception.
+        throw new UsernameNotFoundException("Username: " + username + " not found");
+	}
+	
+	@HystrixCommand(fallbackMethod = "fallback")
+	public Map getUserInfoFromUsersService(String username) {
+
     	Map<String, String> vars = new HashMap<>();
     	vars.put("username", username);
 
     	Map<String, Object> userMap = new HashMap<String, Object>();
     	userMap = restTemplate.getForObject("http://users-service/findByName/{username}", Map.class, vars);
 
-    	if (userMap != null && userMap.values().size() > 0) {
-			List<GrantedAuthority> grantedAuthorities = AuthorityUtils
-                    .commaSeparatedStringToAuthorityList(role);
+		return userMap;
+	}
 
-            // The "User" class is provided by Spring and represents a model class for user to be returned by UserDetailsService
-            // And used by auth manager to verify and check user authentication.
-            return new User((String) userMap.get("username"), encoder.encode((String) userMap.get("password")), grantedAuthorities);
-		}
-
-        // If user not found. Throw this exception.
-        throw new UsernameNotFoundException("Username: " + username + " not found");
-    }
+	public Map fallback(Throwable hystrixCommand) {
+		return null;
+	}
 
 }
