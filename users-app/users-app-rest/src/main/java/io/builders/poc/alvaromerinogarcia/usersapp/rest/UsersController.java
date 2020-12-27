08c9564f3e9d930b9a1618f5d2c4a6b9a0a45ef8
Alvaro Merino Garcia <alvaromerinogarcia@gmail.com>
2020-12-27 11:14:58 +0100

    Rest API of users

---
+++ users-app/users-app-rest/src/main/java/io/builders/poc/alvaromerinogarcia/usersapp/rest/UsersController.java
@@ -0,0 +1,26 @@
+package io.builders.poc.alvaromerinogarcia.usersapp.rest;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import io.builders.poc.alvaromerinogarcia.usersapp.domain.data.UserDto;
+
+public interface UsersController {
+	
+	@PostMapping
+	ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto);
+	
+    @GetMapping("/{userId}")
+    ResponseEntity<UserDto> getUserById(@PathVariable("userId") Integer userId);
+
+    @GetMapping
+    ResponseEntity<List<UserDto>> getUsers();
+
+}
---
+++ users-app/users-app-rest/src/main/java/io/builders/poc/alvaromerinogarcia/usersapp/rest/UsersControllerImpl.java
@@ -0,0 +1,46 @@
+package io.builders.poc.alvaromerinogarcia.usersapp.rest;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.builders.poc.alvaromerinogarcia.usersapp.core.service.UsersService;
+import io.builders.poc.alvaromerinogarcia.usersapp.domain.data.UserDto;
+import io.builders.poc.alvaromerinogarcia.usersapp.domain.exception.UserNotFoundException;
+
+@RestController("/")
+public class UsersControllerImpl implements UsersController {
+
+	private final UsersService usersService;
+
+    public UsersControllerImpl(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+	@Override
+	public ResponseEntity<UserDto> createUser(UserDto userDto) {
+		UserDto u = usersService.addUser(userDto);
+		if (u == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}
+		
+		return new ResponseEntity<>(u, HttpStatus.CREATED);
+	}
+    
+	@Override
+	public ResponseEntity<UserDto> getUserById(Integer userId) {
+		try {
+			return new ResponseEntity<>(usersService.getUserByID(userId), HttpStatus.OK);
+		} catch (UserNotFoundException ex) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@Override
+	public ResponseEntity<List<UserDto>> getUsers() {
+        return new ResponseEntity<>(usersService.getAllUsers(), HttpStatus.OK);
+	}
+
+}
